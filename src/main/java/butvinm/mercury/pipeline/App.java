71cb3d5b6f2de05650d1ff2b936c1f9a1449605b
Mihail Butvin <butvin.mihail@yandex.ru>
2023-10-03 11:45:00 +0300

    Init maven project

---
+++ src/main/java/butvinm/mercury/pipeline/App.java
@@ -0,0 +1,13 @@
+package butvinm.mercury.pipeline;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
