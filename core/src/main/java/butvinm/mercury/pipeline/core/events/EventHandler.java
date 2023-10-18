33dface7e32ebe0eed24a54331cf792ebd7371d9
Mihail Butvin <butvin.mihail@yandex.ru>
2023-10-18 05:24:06 +0300

    Define Either as event hanlder result
    Use Either as EventHandler.handleEvent result to have more precise error
    messaging.

--- core/src/main/java/butvinm/mercury/pipeline/core/events/EventHandler.java
+++ core/src/main/java/butvinm/mercury/pipeline/core/events/EventHandler.java
@@ -1,11 +1,14 @@
 package butvinm.mercury.pipeline.core.events;
 
 import io.vavr.collection.Map;
-import io.vavr.control.Option;
+import io.vavr.control.Either;
 
 /**
  * Handle and parse {@link Event} from environment.
+ *
+ * Environment, actually, can be anything, but generally we meant environment
+ * variables of runner from which pipeline was called.
  */
 public interface EventHandler {
-    Option<Event> handleEvent(Map<String, String> env);
+    Either<String, Event> handleEvent(Map<String, String> env);
 }
