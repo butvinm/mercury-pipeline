f69520bf0a28d280b6244f2c9001bdd3f69d4b19
Mihail Butvin <butvin.mihail@yandex.ru>
2023-11-30 18:45:08 +0300

    Draft GitLab Webhook handler

--- src/main/java/butvinm/mercury/pipeline/PipelineApplication.java
+++ src/main/java/butvinm/mercury/pipeline/PipelineApplication.java
@@ -1,22 +1,51 @@
 package butvinm.mercury.pipeline;
 
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import butvinm.mercury.pipeline.models.MREvent;
+import lombok.val;
 
 @SpringBootApplication
 @RestController
 public class PipelineApplication {
+    private final Logger logger = initLogger();
+
     public static void main(String[] args) {
         SpringApplication.run(PipelineApplication.class, args);
     }
 
-    @GetMapping
-    public String handlerHello(
-        @RequestParam("name") String name
+    @PostMapping("/merge-requests")
+    public String mergeRequestHandler(
+        @RequestBody MREvent event
     ) {
-        return "Heeeeelllo, %s!".formatted(name);
+        logger.info(event.toString());
+        val prevReviewers = event.getChanges().getReviewers().getPrevious();
+        val currReviewers = event.getChanges().getReviewers().getPrevious();
+        if (currReviewers.equals(prevReviewers)) {
+            return "New Reviewer!";
+        }
+        return event.toString();
+    }
+
+    private Logger initLogger() {
+        val logger = Logger.getLogger("main");
+        try {
+            val fileHandler = new FileHandler("/tmp/logs/logs.log");
+            logger.addHandler(fileHandler);
+            val formatter = new SimpleFormatter();
+            fileHandler.setFormatter(formatter);
+            return logger;
+        } catch (SecurityException | IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
